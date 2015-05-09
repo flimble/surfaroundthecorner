627ced0e855294ed13f6e58d96098fd9a24dc74d
John Fattal <john.fattal@foxycode.com>
2015-05-09 22:18:39 +1000

    corrected the mongo connection tring

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/surfaroundthecorner',
+	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://app:app@ds037387.mongolab.com:37387/surfaroundthecorner',
 	assets: {
 		lib: {
 			css: [
