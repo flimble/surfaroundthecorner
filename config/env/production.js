17d780142e9e758a0c6c9d6a423fafa7ee9a9870
John Fattal <john.fattal@foxycode.com>
2015-05-09 23:06:14 +1000

    added missing bower files

--- config/env/production.js
+++ config/env/production.js
@@ -7,6 +7,7 @@ module.exports = {
 			css: [
 				'public/lib/bootstrap/dist/css/bootstrap.min.css',
 				'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+				'public/lib/angular-ui-select/dist/select.min.css'
 			],
 			js: [
 				'public/lib/angular/angular.min.js',
@@ -17,7 +18,10 @@ module.exports = {
 				'public/lib/angular-sanitize/angular-sanitize.js', 
 				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
 				'public/lib/angular-ui-utils/ui-utils.min.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js'
+				'public/lib/jquery/dist/jquery.min.js',
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
+				'public/lib/angular-ui-select/dist/select.min.js',
+				'public/lib/ng-lodash/build/ng-lodash.js'
 			]
 		},
 		css: 'public/dist/application.min.css',
