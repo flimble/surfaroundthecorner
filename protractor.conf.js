4e49069844c111034780620e7cf131b5952cf890
John Fattal <john.fattal@saiglobal.com>
2015-05-15 23:06:35 +1000

    changed browser to firefox

--- protractor.conf.js
+++ protractor.conf.js
@@ -4,7 +4,7 @@ exports.config = {
 
   // Capabilities to be passed to the webdriver instance.
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'firefox'
   },
 
   baseUrl: 'http://localhost:9001',
