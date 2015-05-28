afbca080993b926858e39af81c03b8d12bf58999
John Fattal <john.fattal@saiglobal.com>
2015-05-28 17:26:43 +1000

    forget it

--- gulp/tasks/server.js
+++ gulp/tasks/server.js
@@ -2,15 +2,11 @@
 
 var config = require('../config');
 var globalConfig = require('./loadConfig');
-var http = require('http');
-var express = require('express');
 var gulp = require('gulp');
-var gutil = require('gulp-util');
 var nodemon = require('gulp-nodemon');
 
 
 gulp.task('server', ['loadConfig'], function() {  
-
   nodemon({
       script: 'server.js',
       ext: 'js',
